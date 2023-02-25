73dc9e6237259fb7faf5c58ed8f9198092952838
Gabryel-lima <gabbryellimasi@gmail.com>
2023-02-25 17:04:21 -0300

    Atualizando

--- Strings.py/ClasseExtratorurl.py
+++ Strings.py/ClasseExtratorurl.py
@@ -1,5 +1,6 @@
 import re
 
+
 class ExtratorURL:
     def __init__(self, url):
         self.url = self.sanitiza_url(url)
@@ -16,7 +17,7 @@ class ExtratorURL:
             raise ValueError("A URL está vazia")
 
         padrao_url = re.compile('(http(s)?://)?(www.)?bytebank.com(.br)?/cambio')
-        match = padrao_url.match(self.url)
+        match = padrao_url.match(url)
         if not match:
             raise ValueError("A URL não é válida.")
 
@@ -40,9 +41,26 @@ class ExtratorURL:
             valor = self.get_url_parametros()[indice_valor:indice_e_comercial]
         return valor
 
+    def __len__(self):
+        return len(self.url)
+
+    def __str__(self):
+        return self.url + "\n" + "Parâmetros: " + self.get_url_parametros() + "\n" + "URL Base: " + self.get_url_base()
+
+    def __eq__(self, other):
+        return self.url == other.url
+
 
 url = "bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar"
 extrator_url = ExtratorURL(url)
-valor_quantidade = extrator_url.get_valor_parametro("quantidade")
-print(valor_quantidade)
+extrator_url_2 = ExtratorURL(url)
 
+print("O tamanho da URL é: ", len(extrator_url))
+print("URL completa: ", extrator_url)
+
+# Verifica que duas instâncias com a mesma URL são iguais
+print("extrator_url == extrator_url_2? ", extrator_url == extrator_url_2)
+
+# Busca o valor do parâmetro quantidade
+valor_quantidade = extrator_url.get_valor_parametro("quantidade")
+print("Valor do parâmetro 'quantidade': ", valor_quantidade)
