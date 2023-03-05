97fe9fbaabea3c45b3c9e5c70e6a64799a3e5b7c
Gabryel-lima <gabbryellimasi@gmail.com>
2023-03-04 21:59:53 -0300

    Atualizando

---
+++ Exercícios Python/Exercicios de livro.py
@@ -0,0 +1,12 @@
+
+class fracao:
+
+    def __init__(self,num,den):
+        self._num = num 
+        self._den = den
+        self.__str__(den/num)
+
+    def Soma(self):
+        pass
+    
+help(NotImplemented)
